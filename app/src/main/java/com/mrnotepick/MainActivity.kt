319e358342bcd090c2ab06bd436364fb509f32af
EvgenyKravtsov <kravtsov-android@mail.ru>
2017-05-24 16:15:43 +0300

    24/05/2017 ver.1

--- app/src/main/java/com/mrnotepick/MainActivity.kt
+++ app/src/main/java/com/mrnotepick/MainActivity.kt
@@ -3,13 +3,10 @@ package com.mrnotepick
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-import kotlinx.android.synthetic.main.activity_main.*
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("TEST")
     }
 }
